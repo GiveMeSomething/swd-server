cc51f885a99013a0d117e14fc64f07fc081feb4d
Hoàng Tiến Minh <htminh.1001@gmail.com>
2022-10-24 10:53:18 +0700

    chore: setup email and class-validator dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MailService } from './mail/mail.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MailService],
 })
 export class AppModule {}
